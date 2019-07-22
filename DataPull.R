f27555c6a7013f7784af3312120312106f97bf6f
gpointonSOR <grant.pointon@sorensonimpact.com>
2019-07-22 14:45:26 -0600

    get_acs

---
+++ DataPull.R
@@ -0,0 +1,9 @@
+
+
+
+
+thing <- map_df(us, function(x) {
+  get_acs(geography = "tract", variables = c("B15001_006", "B15001_007", "B15001_008", 
+                                             "B15001_009", "B15001_010"), 
+          state = x)
+})
