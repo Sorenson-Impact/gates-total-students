f984a3f8027b50683575d6d645884b1ea7be9606
Daniel Hadley <dphnrome@gmail.com>
2019-08-08 12:16:12 -0600

    code for scraping 2005 onwards

--- birth_rate_cleaning.R
+++ birth_rate_cleaning.R
@@ -1,5 +1,3 @@
-# birth rate data -- from Jon Pritzker community assessment tool
-
 library(janitor)
 library(readxl)
 library(fs)
@@ -9,47 +7,7 @@ library(tidyverse)
 library(purrr)
 library(tidycensus)
 
-si_ggplot_theme_update()
-si_knitr_settings()
-gdrive_dir <- "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData"
-
-load("~/Google Drive/SI/DataScience/Pritzker/ACS Data/geoids lookup table.RData")
-
-# Get wondr data
-# Settings: select years individually
-# use bridged race because it has fewer categories / fewer suppressions
-
-files <- dir_ls(path(gdrive_dir),glob = "*.txt")
-wonder <- files %>% 
-  map(function(x) {
-    suppressWarnings(read_tsv(x)) %>% #na = c("", "NA", "Suppressed"), col_types = cols(Births = "i")
-      clean_names() %>% 
-      filter(!is.na(county)) %>% 
-      select(-notes) %>% 
-      separate(col = county, into = c("county", "state"), sep = ", ") %>% 
-      
-      rename(geoid = county_code) %>% 
-      
-      select(-contains("_code")) %>% 
-      add_column(source = str_sub(basename(x), end = -5)) %>% 
-      separate(source, into = c("data", "subgroup"), sep = " X ")
-  } 
-  ) %>% set_names(str_sub(basename(files), end = -5))
-
-#Number of births without race/ethnicity
-all_births <- wonder %>% 
-  reduce(bind_rows) %>% 
-  filter(year <= 1997)
-
-#   select(-subgroup) %>% 
-#   left_join(filter(., data == "all births") %>%
-#               select(geoid, year, all_births = births)) %>%
-#   mutate() %>% 
-#   full_join(geoids$county %>% select(-state_name, -name_county), by = c("geoid" = "GEOID", "county", "state")) %>% 
-#   complete(year, nesting(county, state, geoid), data) %>% 
-#   filter(!is.na(year), !is.na(data)) %>% 
-#   mutate_all(funs(replace_na(., "Data Unavailable")))
-
+# i am so sorry, i couldnt figure out how to automate ... :( -- Lily
 natl1988 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1988.csv")
 natl1989 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1989.csv")
 natl1990 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1990.csv")
@@ -59,9 +17,30 @@ natl1995 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/Bi
 natl1994 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1994.csv")
 natl1993 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1993.csv")
 natl1992 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1992.csv")
+natl1998 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1998.csv")
+natl1999 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1999.csv")
+natl1999 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/natl1999.csv")
+natl2000 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2000.csv")
+natl2001 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2001.csv")
+natl2002 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2002.csv")
+natl2003 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2003.csv")
+natl2004 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2004.csv")
+natl2005 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2005.csv")
+natl2006 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2006.csv")
+natl2007 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2007.csv") ##
+natl2008 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2008.csv")
+natl2009 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2009.csv")
+natl2010 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2010.csv")
+natl2011 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2011.csv")
+natl2012 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2012.csv")
+natl2013 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2013.csv")
+natl2014 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2014.csv")
+natl2015 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2015.csv")
+natl2016 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2016.csv")
+natl2017 <- read_csv("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/future birth rate data/natl2017.csv")
 
 fips <- fips_codes %>% 
-  select(state, state_code) %>% 
+  select(state_name, state) %>% 
   unique()
 
 births88 <- natl1988 %>% 
@@ -136,6 +115,169 @@ births92 <- natl1992 %>%
   select(-c(stoccfip)) %>% 
   left_join(fips)
 
+births98 <- natl1998 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 1998, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births99 <- natl1999 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 1999, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births00 <- natl2000 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2000, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births01 <- natl2001 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2001, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births02 <- natl2002 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2002, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births03 <- natl2003 %>% 
+  group_by(ostate) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2003, state_code = ostate) %>% 
+  select(-c(ostate))
+
+births04 <- natl2004 %>% 
+  group_by(ostate) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2004, state_code = ostate) %>% 
+  select(-c(ostate))
+
+births05 <- natl2005 %>% 
+  group_by(xostate) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2005, state_code = xostate) %>% 
+  select(-c(xostate))
+
+births06 <- natl2006 %>% 
+  group_by(ostate) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2006, state_code = ostate) %>% 
+  select(-c(ostate))
+
+births07 <- natl2007 %>% 
+  group_by(ostate) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2007, state_code = ostate) %>% 
+  select(-c(ostate))
+
+births08 <- natl2008 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2008, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births09 <- natl2009 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2009, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births10 <- natl2010 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2010, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births11 <- natl2011 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2011, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births12 <- natl2012 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2012, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births13 <- natl2013 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2013, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births14 <- natl2014 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2014, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births15 <- natl2015 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2015, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births15 <- natl2015 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2015, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births16 <- natl2016 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2016, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
+births17 <- natl2017 %>% 
+  group_by(stoccfip) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(datayear = 2017, state_code = stoccfip) %>% 
+  select(-c(stoccfip)) %>% 
+  left_join(fips)
+
 write_rds(births88, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births88.rds")
 write_rds(births89, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births89.rds")
 write_rds(births90, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births90.rds")
@@ -145,32 +287,89 @@ write_rds(births95, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/Bir
 write_rds(births94, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births94.rds")
 write_rds(births93, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births93.rds")
 write_rds(births92, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births92.rds")
+write_rds(births98, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births98.rds")
+write_rds(births99, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births99.rds")
+write_rds(births00, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births00.rds")
+write_rds(births01, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births01.rds")
+write_rds(births02, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births02.rds")
+write_rds(births03, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births03.rds")
+write_rds(births04, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births04.rds")
+write_rds(births05, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births05.rds")
+write_rds(births06, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births06.rds")
+write_rds(births07, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births07.rds")
+write_rds(births08, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births08.rds")
+write_rds(births09, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births09.rds")
+write_rds(births10, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births10.rds")
+write_rds(births11, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births11.rds")
+write_rds(births12, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births12.rds")
+write_rds(births13, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births13.rds")
+write_rds(births14, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births14.rds")
+write_rds(births15, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births15.rds")
+write_rds(births16, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births16.rds")
+write_rds(births17, "/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births17.rds")
 
-# lily playing around
-births88 <- read_rds("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births88.rds")
-births89 <- read_rds("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births89.rds")
-births90 <- read_rds("/Volumes/GoogleDrive/My Drive/SI/DataScience/data/gates/BirthData/births90.rds")
+# 2005-2017, state data not available on the national dataset, have to go to the PDF description, copy, paste, and scrape... URGH NBER AND CDC URGH
 
-rm(natl1988)
-rm(natl1989)
-rm(natl1990)
+births_2005 = "Alabama 59,300 60,453
+Alaska 10,365 10,459
+Arizona 95,687 96,199
+Arkansas 38,381 39,208
+California 549,100 548,882
+Colorado 69,205 68,944
+Connecticut 42,133 41,718
+Delaware 12,265 11,643
+District of Columbia 14,311 7,971
+Florida 226,415 226,240
+Georgia 143,476 142,200
+Hawaii 17,911 17,924
+Idaho 22,522 23,062
+Illinois 175,714 179,020
+Indiana 87,843 87,193
+Iowa 39,337 39,311
+Kansas 40,737 39,888
+Kentucky 54,590 56,444
+Louisiana 60,461 60,937
+Maine 13,975 14,112
+Maryland 71,292 74,980
+Massachusetts 77,820 76,865
+Michigan 126,498 127,706
+Minnesota 70,933 70,919
+Mississippi 41,175 42,395
+Missouri 79,523 78,618
+Montana 11,551 11,583
+Nebraska 26,350 26,145
+Nevada 36,950 37,268
+New Hampshire 13,968 14,420
+New Jersey 110,800 113,776
+New Mexico 28,291 28,835
+New York 247,901 246,351
+North Carolina 123,943 123,096
+North Dakota 9,621 8,390
+Ohio 148,876 148,388
+Oklahoma 50,656 51,801
+Oregon 46,712 45,922
+Pennsylvania 144,908 145,383
+Rhode Island 13,481 12,697
+South Carolina 55,321 57,711
+South Dakota 11,957 11,462
+Tennessee 87,072 81,747
+Texas 387,856 385,915
+Utah 52,555 51,556
+Vermont 5,932 6,295
+Virginia 102,646 104,555
+Washington 82,336 82,703
+West Virginia 21,150 20,836
+Wisconsin 69,769 70,984
+Wyoming 6,778 7,239"
 
-births88 <- births88 %>% 
-  rename(births_1988 = n) %>% 
-  select(-c(datayear))
-births89 <- births89 %>% 
-  rename(births_1989 = n) %>% 
-  select(-c(datayear))
-births90 <- births90 %>% 
-  rename(births_1990 = n) %>% 
-  select(-c(datayear))
+births05 <- str_split(births_2005, "\\n", simplify = TRUE) %>%
+  as_tibble() %>%
+  gather(key = "key", value = "value") %>%
+  select(-c(key)) %>%
+  extract(value, into = c("state_name", "n", "residence"), regex = "([A-z\\s]*)\\s([\\d\\,]+)\\s([\\d\\,]+)") %>% 
+  mutate(n = as.integer(gsub(",","",n))) %>% 
+  left_join(fips) %>% 
+  select(-c(residence, state_name)) %>% 
+  rename("state_code" = "state") %>% 
+  mutate(datayear = 2005)
 
-birthdata <- bind_rows(births88, births89, births90)
-
-birthdata <- birthdata %>% 
-  mutate(t18 = datayear + 18,
-         t19 = datayear+19,
-         t20 = datayear+20)
-
-t <- left_join(births88, births89, by = c("state", "state_code")) %>%
-  left_join(births90)
